43ea0e378809630777173f7cebcbb0124f91e70d
Sagar <sagar.sharma2177@gmail.com>
2021-08-04 16:50:57 +0530

    Update index.js

--- Day7-Day8/index.js
+++ Day7-Day8/index.js
@@ -70,19 +70,15 @@ function Volume (height, radius) {
 var library = [
     { title: "The Road Ahead", author: "Bill Gates", libraryID: 1254 },
     { title: "Walter Isaacson", author: "Steve Jobs", libraryID: 4264 },
-    {
-      title: "Mockingjay: The Final Book of The Hunger Games",
+    {title: "Mockingjay: The Final Book of The Hunger Games",
       author: "Suzanne Collins",
-      libraryID: 3245,
-    },
+      libraryID: 3245},
   ];
-  
   library.forEach((item) => {
   
     var allItems = Object.keys(item).sort();
       allItems.forEach((i) => {
         console.log(i, item[i]);
       });
-
   });
 
